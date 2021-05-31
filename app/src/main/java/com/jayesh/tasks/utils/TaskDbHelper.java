6c5eb6d288ce6e8ca90eda035dcec6ba49504734
jayesh-trivedi <jayesh.trvdi@gmail.com>
2021-05-31 18:26:58 +0530

    added constants for column names

--- app/src/main/java/com/jayesh/tasks/utils/TaskDbHelper.java
+++ app/src/main/java/com/jayesh/tasks/utils/TaskDbHelper.java
@@ -12,19 +12,38 @@ public class TaskDbHelper extends SQLiteOpenHelper {
     public static final String TBL_TASK = "tasks";
     public static final String TBL_USER = "users";
 
+    public static final String TASKS_ID = "id";
+    public static final String TASKS_TITLE = "title";
+    public static final String TASKS_USER_ID_FK = "userId";
+    public static final String USER_ID = "id";
+    public static final String USER_USERNAME = "userName";
+
     public TaskDbHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
+    }
+
+    @Override
     public void onCreate(SQLiteDatabase db) {
-        String createUserTable = "CREATE TABLE " + TBL_USER + " ( _ID INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT NOT NULL UNIQUE);";
-        String createTaskTable = "CREATE TABLE " + TBL_TASK + " ( _ID INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT NOT NULL UNIQUE, userID INTEGER NOT NULL, FOREIGN KEY (userID) REFERENCES " + TBL_USER + " (_ID));";
+        String createUserTable = "CREATE TABLE " + TBL_USER + " ( " + USER_ID + " INTEGER PRIMARY KEY" +
+                " AUTOINCREMENT, " + USER_USERNAME + " TEXT NOT NULL UNIQUE);";
+        String createTaskTable = "CREATE TABLE " + TBL_TASK + " ( " + TASKS_ID + " INTEGER PRIMARY KEY " +
+                "AUTOINCREMENT, " + TASKS_TITLE + " TEXT NOT NULL UNIQUE, " +
+                TASKS_USER_ID_FK + " INTEGER NOT NULL, FOREIGN KEY (" +
+                TASKS_USER_ID_FK + ") REFERENCES " + TBL_USER + " (" + USER_ID + "));";
         db.execSQL(createUserTable);
         db.execSQL(createTaskTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TBL_USER);
+        db.execSQL("DROP TABLE IF EXISTS " + TBL_TASK);
+        onCreate(db);
     }
 }
